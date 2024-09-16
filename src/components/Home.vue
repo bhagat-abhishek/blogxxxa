7e87368d1877970708c95a22de243f10c65c5ca4
bhagat-abhishek <inst.abhishek@gmail.com>
2024-09-16 16:08:22 +0530

    ..

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>if else Conditions</h1>
+
+    <h3 v-if="show">if Condition</h3>
+    <h3 v-else>else</h3>
+
+    <button v-on:click="show=!show" >Show</button>    
+    
+    
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            show: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #f69;
+}
+</style>
